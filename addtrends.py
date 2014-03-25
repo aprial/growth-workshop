a738968dd884b7a79712b2418860f4181e9eaf1b
agibsonccc <agibson@clevercloudcomputing.com>
2014-03-24 19:49:47 -0700

    scaffolding for database updates, still going to stew on the biases as of yet, still dont quite understand sqlalchemy with pandas well enough to make educated decisions

--- addtrends.py
+++ addtrends.py
@@ -9,7 +9,21 @@ Biases -
 
 Randomly add bias such that users from twitter do not convert as much.
 Make pinterest users the ones who tend to buy things
-Ensure lots of traffic from facebook and twitter, slightly more conversions from facebook than twitter.__add__(
+Ensure lots of traffic from facebook and twitter, slightly more conversions from facebook than twitter.
 Lots of liking and sharing on reddit and facebook.
 Possibly add slightly more shares for Instagram.
+
+
+
 """
+
+metadata = BoundMetaData(db)
+
+
+users = Table('Users', metadata, autoload=True)
+meals = Table('Meal',metadata ,autoload = True)
+events = Table('Event',metadata,autoload = True)
+
+
+
+
